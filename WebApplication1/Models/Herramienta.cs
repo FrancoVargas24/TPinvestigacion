0abe7c9d084a2c1349a8674c4d3ed4d44f177099
NotaMateria <fran.caco.fv@gmail.com>
2026-05-03 19:56:49 -0300

    ejercicio Herramientas

---
+++ WebApplication1/Models/Herramienta.cs
@@ -0,0 +1,10 @@
+namespace WebApplication1.Models
+{
+    public class Herramienta
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public string Imagen { get; set; }
+    }
+}
