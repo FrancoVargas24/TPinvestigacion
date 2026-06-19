f4956310d9fe2d29d957d14de9f9594694d51665
MartB <mbogadufour@alumno.unlam.edu.ar>
2026-06-19 01:01:56 -0300

    se agregan vistas Home y Error, controlador HomeController, estilos de css, img

--- Entidad/obj/Debug/net10.0/Entidad.AssemblyInfo.cs
+++ Entidad/obj/Debug/net10.0/Entidad.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2745e2b43f83a8434df653c9f59277dcdaff6d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
