8a5cbfc6ecad70ac3910277f44deb4c4e7c4dc3a
MartB <mbogadufour@alumno.unlam.edu.ar>
2026-06-28 23:18:44 -0300

    mejora del home y visualizacion de ultimas publicaciones

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,14 +1,33 @@
-using System.Diagnostics;
+using Entidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using WebApplication1.Models;
+using WebApplication1.Models.ViewModels;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly MusicTradeDbContext _context;
+
+        public HomeController(MusicTradeDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new HomeViewModel
+            {
+                UltimasPublicaciones = await _context.Publicaciones
+                    .Include(p => p.Usuario)
+                    .OrderByDescending(p => p.FechaCreacion)
+                    .Take(3)
+                    .ToListAsync()
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
@@ -19,7 +38,10 @@ namespace WebApplication1.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
